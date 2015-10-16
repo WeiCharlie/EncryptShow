f33f2d9919ce9eb3fbe88317e973534de5944d1e
Charlie Wei <charlie_net@163.com>
2015-10-16 22:20:27 +0800

    本次提交添加了测试公钥私钥Base64等布局

---
+++ src/com/charlie/encryptshow/RSAActivity.java
@@ -0,0 +1,84 @@
+package com.charlie.encryptshow;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created
+ * Author:Charlie Wei[]
+ * Email:charlie_net@163.com
+ * Date:2015/10/16
+ */
+public class RSAActivity extends Activity {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rsa_activity);
+    }
+
+    /**
+     * 将解密内容显示到文本
+     * @param view
+     */
+    public void decryptToText(View view) {
+    }
+
+    /**
+     * 将加密内容显示到文本
+     * @param view
+     */
+    public void encryptToText(View view) {
+    }
+
+    /**
+     * 解密
+     * @param view
+     */
+    public void btnRSADecrypt(View view) {
+    }
+
+    /**
+     * 加密
+     * @param view
+     */
+    public void btnRSAEncrypt(View view) {
+    }
+
+    /**
+     *
+     * @param view
+     */
+
+
+    /**
+     * 公钥转换Base64
+     * @param view
+     */
+    public void btnPublicToRSADecrypt(View view) {
+    }
+
+    /**
+     * 形成Json到文件
+     * @param view
+     */
+    public void btnJsonTOFile(View view) {
+    }
+
+    /**
+     * RSA私钥加密
+     * @param view
+     */
+    public void btnPrivateRSAEncrypt(View view) {
+    }
+
+    /**
+     * 内容转换成Base64
+     * @param view
+     */
+    public void btnTextToRSAEncrypt(View view) {
+    }
+}
